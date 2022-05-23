bc4f533669822acaeeddf3645dc65667c612d953
agan95 <nyonggpj9995@gmail.com>
2022-05-23 21:39:20 +0700

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+// import
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+const port = process.env.APP_PORT || 3000;
+
+//midleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.get("/", (req, res) => {
+  res.send("oke baik");
+});
+
+//error handling
+app.use((req, res, next) => {
+  let err = new Error(`${req.method} => ${req.path} not found`);
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500).json({
+    status: "failed",
+    message: err.message,
+  });
+});
+
+// run server
+app.listen(port, () => {
+  console.log(`app listen on port ${port}`);
+});
