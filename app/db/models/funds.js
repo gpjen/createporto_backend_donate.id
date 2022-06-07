3c937a1a69baf8d5227b58dea992eba08e45e123
agan95 <nyonggpj9995@gmail.com>
2022-06-08 01:58:59 +0900

    create associate model with other table

--- app/db/models/funds.js
+++ app/db/models/funds.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class funds extends Model {
     /**
@@ -10,17 +8,39 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      funds.hasMany(models.payments, {
+        as: "payment",
+        foreignKey: {
+          name: "idFund",
+        },
+      });
+
+      funds.hasMany(models.images_thumbnail, {
+        as: "img-thumb",
+        foreignKey: {
+          name: "idFund",
+        },
+      });
+
+      funds.belongsTo(models.users, {
+        as: "user",
+        foreignKey: {
+          name: "id",
+        },
+      });
     }
   }
-  funds.init({
-    tittle: DataTypes.STRING,
-    goal: DataTypes.INTEGER,
-    desc: DataTypes.TEXT,
-    idUser: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'funds',
-  });
+  funds.init(
+    {
+      tittle: DataTypes.STRING,
+      goal: DataTypes.INTEGER,
+      desc: DataTypes.TEXT,
+      idUser: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "funds",
+    }
+  );
   return funds;
 };
