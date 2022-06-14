f354ddfc9a7d729b3031cf5a588fe8f41e923097
agan95 <nyonggpj9995@gmail.com>
2022-06-15 05:34:43 +0900

    update

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-  console.log("> ", error.message);
+  console.log("> ", error?.message);
   res.status(err.status || 500).json({
     status: "failed",
     message: err.message,
