2ebd2e0ef68180e895cdb25579d922528b1fb948
agan95 <nyonggpj9995@gmail.com>
2022-06-17 10:56:50 +0900

    add config multer update

--- src/midleware/multerConfig.js
+++ src/midleware/multerConfig.js
@@ -33,3 +33,33 @@ exports.imgThumbUpload = multer({
     fileSize: 5 * 1024 * 1024,
   },
 }).array("img_thumb", 3);
+
+// set filter update file
+const imgFilterUpdate = (req, file, cb) => {
+  if (
+    file.fieldname === "img1" ||
+    file.fieldname === "img2" ||
+    file.fieldname === "img3"
+  ) {
+    if (!file.originalname.match(/\.(jpg|png|JPG|JPEG|PNG|gif|GIF)$/)) {
+      req.fileVallidationError = `${file.originalname} not supported format`;
+      return cb(new Error(`format type not supported`), false);
+    }
+  } else {
+    cb(new Error(`diferent field image`), false);
+  }
+  cb(null, true);
+};
+
+//img-thumbnail update img thumb
+exports.imgThumbUpdate = multer({
+  storage,
+  fileFilter: imgFilterUpdate,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+}).fields([
+  { name: "img1", maxCount: 1 },
+  { name: "img2", maxCount: 1 },
+  { name: "img3", maxCount: 1 },
+]);
